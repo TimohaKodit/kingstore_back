d93d2a58dc441a6bc8f07894f070faa36faf7613
TimohaKodit <63901163+TimohaKodit@users.noreply.github.com>
2025-11-13 22:33:34 +0500

    Delete app directory

--- app/dependencies.py
+++
@@ -1,9 +0,0 @@
-from app.db.session import SessionLocal
-
-def get_db():
-    """Зависимость для получения сессии базы данных."""
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
